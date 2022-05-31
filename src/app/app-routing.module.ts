6afb9267d5d226eca9395f964c88d1cb546daec9
lotfi bannouri <lotfi91.contact@gmail.com>
2022-06-01 01:05:19 +0200

    second commit 1/5/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "utilisateur", component: UtilisateurComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    UtilisateurComponent
   ],
   imports: [
     BrowserModule,
